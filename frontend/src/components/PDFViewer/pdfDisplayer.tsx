40135ea611177f711ae69b93652cfa2fef18ddb0
AloPedrero <pedreromartinezalonso@gmail.com>
2026-05-15 02:09:33 -0600

    feat: added pdf web display

--- frontend/src/components/PDFViewer/pdfDisplayer.tsx
+++ frontend/src/components/PDFViewer/pdfDisplayer.tsx
@@ -1,7 +1,17 @@
-import { useMemo } from 'react';
-import { PDFViewer } from '@embedpdf/react-pdf-viewer';
-import TextType from '../../reactbits/components/TextType';
 import React from 'react';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+
+import TextType from '../../reactbits/components/TextType';
+
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 type PDFDisplayerProps = {
   file: File | null;
@@ -11,39 +21,28 @@ const PDFDisplayer = ({
   file,
 }: PDFDisplayerProps) => {
 
-  const pdfUrl = useMemo(() => {
-    if (!file) return null;
-
-    return URL.createObjectURL(file);
-  }, [file]);
-
-  console.log(pdfUrl);
-
   return (
-    <div className="flex h-screen w-[480px] items-center justify-center overflow-hidden rounded-xl border border-neutral-300 bg-neutral-50">
+    <div className="flex h-screen w-[480px] items-center justify-center overflow-auto rounded-xl border border-neutral-300 bg-neutral-50 p-4">
 
-      {pdfUrl ? (
-        <PDFViewer
-          config={{
-            src: pdfUrl,
-
-            theme: {
-              preference: 'light',
-            },
-          }}
-        />
+      {file ? (
+        <Document file={file}>
+          <Page pageNumber={1} />
+        </Document>
       ) : (
-        <p className="text-neutral-500">
-            <TextType 
-            text={["Welcome to CVSync", "Click the button and upload your CV!"]}
+        <div className="text-neutral-500 text-center">
+          <TextType
+            text={[
+              "Welcome to CVSync",
+              "Click the button and upload your CV!"
+            ]}
             typingSpeed={75}
             pauseDuration={1500}
             showCursor
             cursorCharacter="_"
             deletingSpeed={50}
             cursorBlinkDuration={0.5}
-            />
-        </p>
+          />
+        </div>
       )}
 
     </div>
