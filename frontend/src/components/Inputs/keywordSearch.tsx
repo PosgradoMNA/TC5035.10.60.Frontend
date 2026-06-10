68a1285d58080f86ba17760692b5bbf63f82bbcb
AloPedrero <pedreromartinezalonso@gmail.com>
2026-06-10 17:04:26 -0600

    feat: updated into sepaarate parts the accordion and the search up bar

--- frontend/src/components/Inputs/keywordSearch.tsx
+++ frontend/src/components/Inputs/keywordSearch.tsx
@@ -4,60 +4,65 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "../../reactbits/components/ui/input-group"
-import { SearchIcon } from 'lucide-react'
-import React, {useState} from "react"
-import {keywordsToLambda} from "../../services/sendKeywordsToLambda"
+import { SearchIcon } from "lucide-react"
+import React, { useState } from "react"
+import { keywordsToLambda } from "../../services/sendKeywordsToLambda"
 import JobsAccordion from "../Buttons/accordionButton"
-import type { Job } from "../../types/Job";
+import type { Job } from "../../types/Job"
 
 type PDFDisplayerProps = {
-  file: File | null;
-}; 
+  file: File | null
+}
 
 export function AIRecommendations({
   file,
 }: PDFDisplayerProps) {
   const [keywords, setKeywords] = useState("")
   const [jobs, setJobs] = useState<Job[]>([])
+
   const handleSubmit = async () => {
-    if (!file) return;
+    if (!file) return
 
     try {
-      const response = await keywordsToLambda(
-        file,
-        keywords
-      );
+      const response = await keywordsToLambda(file, keywords)
 
-      console.log(response);
+      console.log(response)
 
-      setJobs(response.job_list.response.results || []);
-
+      setJobs(response.job_list.response.results || [])
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   return (
-    <Field className="max-w-xs">
-      <InputGroup>
-        <InputGroupAddon>
-          <SearchIcon />
-        
-        </InputGroupAddon>
-        <InputGroupInput
-          placeholder="Search the job you want..."
-          onChange={(e) => setKeywords(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSubmit()
-            }
-          }}/>
-      </InputGroup>
+    <div className="flex flex-col h-full w-full">
+      
+      {/* Buscador fijo */}
+      <div className="mb-4">
+        <Field>
+          <InputGroup>
+            <InputGroupAddon>
+              <SearchIcon />
+            </InputGroupAddon>
 
-      <JobsAccordion
-        jobs={jobs}
-        />
+            <InputGroupInput
+              placeholder="Search the job you want..."
+              onChange={(e) => setKeywords(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit()
+                }
+              }}
+            />
+          </InputGroup>
+        </Field>
+      </div>
 
-    </Field>
+      {/* Resultados con scroll */}
+      <div className="flex-1 overflow-y-auto border rounded-md p-2">
+        <JobsAccordion jobs={jobs} />
+      </div>
+
+    </div>
   )
 }
